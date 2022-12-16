9686ca4124befa606272dbe4f3dac08109577593
Tomás Vidal <vidaltomas00@gmail.com>
2022-12-16 13:05:22 -0300

    Create webpage styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import "./App.css";
+
 function App() {
   return (
     <div className="App">
-      <h1>Hola</h1>
+      <h1>TODO LIST</h1>
+      <form className="input-form">
+        <input className="input-form-todo" type="text"></input>
+        <button>
+          <span>+</span>
+        </button>
+      </form>
     </div>
   );
 }
