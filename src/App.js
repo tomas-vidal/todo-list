3910946501c41ef1dca9e5ed467e71232afe532d
Tomás Vidal <vidaltomas00@gmail.com>
2022-12-16 21:01:22 -0300

    Add Todo
    Add todo item to list

--- src/App.js
+++ src/App.js
@@ -1,18 +1,49 @@
 import "./App.css";
 import TodoList from "./components/TodoList.js";
+import React, { useState } from "react";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  const [writingText, setWritingText] = useState("");
+
+  const [newTodo, setNewTodo] = useState({
+    text: writingText,
+    isDone: false,
+  });
+
+  const addTodo = (e) => {
+    e.preventDefault();
+    setTodos([
+      ...todos,
+      {
+        ...newTodo,
+      },
+    ]);
+    setWritingText("");
+  };
+
+  const handleChange = (e) => {
+    setWritingText(e);
+    setNewTodo({ ...newTodo, text: writingText });
+  };
+
   return (
     <div className="App">
       <div>
         <h1>TODO LIST</h1>
-        <form className="input-form">
-          <input className="input-form-todo" type="text"></input>
-          <button>
+        <form className="input-form" onSubmit={(e) => addTodo(e)}>
+          <input
+            className="input-form-todo"
+            type="text"
+            value={writingText}
+            onChange={(e) => handleChange(e.target.value)}
+          ></input>
+          <button type="submit">
             <span>+</span>
           </button>
         </form>
-        <TodoList />
+        <TodoList todos={todos} setTodos={setTodos} />
       </div>
     </div>
   );
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,11 +1,17 @@
 import React from "react";
 import "../App.css";
 
-function TodoList() {
+function TodoList({ todos, setTodos }) {
   return (
-    <div className="input-check">
-      <input type="checkbox" id="scales" name="todo-item" />
-      <a classname="todo-name">aaaaa</a>
+    <div>
+      {todos.map((todo) => {
+        return (
+          <div className="input-check">
+            <input type="checkbox" id="scales" name="todo-item" />
+            <a classname="todo-name">{todo.text}</a>
+          </div>
+        );
+      })}
     </div>
   );
 }
